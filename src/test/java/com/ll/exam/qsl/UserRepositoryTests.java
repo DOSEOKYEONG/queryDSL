a63406f60d91e9a56a51369207e5850e0737f2ad
DOSEOKYEONG <dho88dho@naver.com>
2022-09-13 15:09:11 +0900

    9강끝

--- src/test/java/com/ll/exam/qsl/UserRepositoryTests.java
+++ src/test/java/com/ll/exam/qsl/UserRepositoryTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
 
@@ -24,4 +25,19 @@ class UserRepositoryTests {
 		userRepository.saveAll(Arrays.asList(siteUser1, siteUser2));
 	}
 
+	@Test
+	@DisplayName("1번 회원을 Qsl로 가져오기")
+	void t2() {
+		SiteUser siteUser1 = new SiteUser(null, "user1", "{noop}1234", "user1@test.com");
+		SiteUser siteUser2 = new SiteUser(null, "user2", "{noop}1234", "user2@test.com");
+
+		userRepository.saveAll(Arrays.asList(siteUser1, siteUser2));
+
+		SiteUser u1 = userRepository.getQslUser(1L);
+
+		assertThat(u1.getId()).isEqualTo(1L);
+		assertThat(u1.getUsername()).isEqualTo("user1");
+		assertThat(u1.getEmail()).isEqualTo("user1@test.com");
+		assertThat(u1.getPassword()).isEqualTo("{noop}1234");
+	}
 }
